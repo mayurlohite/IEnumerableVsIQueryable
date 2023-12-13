55af1f46a4adf0ee3bb0caef47cfc4e5f7856ce5
Mayur Lohite <mayee.28@gmail.com>
2023-12-13 10:04:47 +0530

    Added project -  IEnumerable Vs IQueryable

---
+++ IEnumerableVsIQueryable/Program.cs
@@ -0,0 +1,26 @@
+﻿using IEnumerableVsIQueryable;
+using Microsoft.EntityFrameworkCore;
+
+namespace IEnumerableVsIQueryable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new BookDemoDbContext())
+            {
+
+                var booksIEnumerable = (from book in context.Books
+                                        select book)
+                                        .AsEnumerable<Book>().Take(2).ToList();
+
+
+                var booksIQurable = (from book in context.Books
+                                     select book)
+                                     .AsQueryable<Book>().Take(2).ToList();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
